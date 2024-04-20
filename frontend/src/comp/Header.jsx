7339df631dcc8fe3b31b058a612e1d35b19012cf
itsManish01 <manishkumarp0105@gmail.com>
2024-04-20 08:01:00 +0530

    some changes

--- frontend/src/comp/Header.jsx
+++ frontend/src/comp/Header.jsx
@@ -22,9 +22,10 @@ export default function Header() {
       <header class="text-gray-400 bg-gray-900 body-font">
       <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-          <Link to="/create/snippet" class="mr-5 hover:text-white">Create Snippet</Link>
-          <a class="mr-5 hover:text-white">Share Snippet</a>
-          <a class="mr-5 hover:text-white">Add Snippet</a>
+          <Link to="/create/snippet" class="mr-5 hover:text-white">Create Snip</Link>
+          <a class="mr-5 hover:text-white">My Snip</a>
+          <a class="mr-5 hover:text-white">Default Snip</a>
+          <a class="mr-5 hover:text-white">Add Snip</a>
         </nav>
         <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-4 md:mb-0">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-orange-500">
